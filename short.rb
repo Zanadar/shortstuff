31bef78d2a4b37485ff1245922b920cac03f9e50
Zander Mackie <zmackie@gmail.com>
2016-02-19 14:50:14 -0500

    Error Handling

--- short.rb
+++ short.rb
@@ -25,23 +25,27 @@ end
 post '/s' do
   @key = SecureRandom.urlsafe_base64(7)
   url =  params[:url]
-  @path = gen_url(url)
+  path = gen_url(url)
   store.transaction do
-    store[@key] = @path
+    store[@key] = path
   end
   haml :response
 end
 
+error URI::InvalidComponentError do
+  haml :error
+end
+
+not_found do
+  haml :error
+end
+
 ## Best effort to make a valid url
 def gen_url(url)
-  begin
-    if url =~ /\A#{URI::regexp(['http', 'https'])}\z/
-      return url
-    else
-      return URI::HTTP.build(host: url).to_s
-    end
-  rescue
-    halt "That's a bad url!"
+  if url =~ /\A#{URI::regexp(['http', 'https'])}\z/
+    return url
+  else
+    return URI::HTTP.build(host: url).to_s
   end
 end
 
